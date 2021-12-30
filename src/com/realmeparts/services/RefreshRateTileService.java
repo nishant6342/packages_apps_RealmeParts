61544f7b33f9fadfab4360ac812eafbbc89ef561
Nishant Kumar <www.rajsonu13@gmail.com>
2021-12-30 15:24:54 +0530

    RealmeParts: Nuke model check in quick settings tiles
    * This repo is specific for RMX3031
    * Also nuke CABC and perf profile tiles
    
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>

--- src/com/realmeparts/services/RefreshRateTileService.java
+++ src/com/realmeparts/services/RefreshRateTileService.java
@@ -50,17 +50,13 @@ public class RefreshRateTileService extends TileService {
     public void onStartListening() {
         super.onStartListening();
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
-        if (!sharedPrefs.getBoolean("refresh_rate_90_device", false)) {
-            getQsTile().setState(Tile.STATE_UNAVAILABLE);
-            getQsTile().setLabel(getResources().getString(R.string.unsupported));
-        } else {
-            enabled = RefreshRateSwitch.isCurrentlyEnabled(this);
-            RefreshRateSwitch.setPeakRefresh(this, enabled);
-            getQsTile().setIcon(Icon.createWithResource(this,
-                    GetSmoothDisplay() ? R.drawable.refresh_rate_90forced_icon : (enabled ? R.drawable.ic_refresh_tile_90 : R.drawable.ic_refresh_tile_60)));
-            getQsTile().setState(GetSmoothDisplay() ? Tile.STATE_ACTIVE : (enabled ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE));
-            getQsTile().setLabel(GetSmoothDisplay() ? "Smooth Display" : "Refresh Rate");
-        }
+
+        enabled = RefreshRateSwitch.isCurrentlyEnabled(this);
+        RefreshRateSwitch.setPeakRefresh(this, enabled);
+        getQsTile().setIcon(Icon.createWithResource(this,
+            GetSmoothDisplay() ? R.drawable.refresh_rate_90forced_icon : (enabled ? R.drawable.ic_refresh_tile_90 : R.drawable.ic_refresh_tile_60)));
+        getQsTile().setState(GetSmoothDisplay() ? Tile.STATE_ACTIVE : (enabled ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE));
+        getQsTile().setLabel(GetSmoothDisplay() ? "Smooth Display" : "Refresh Rate");
         getQsTile().updateTile();
     }
 
